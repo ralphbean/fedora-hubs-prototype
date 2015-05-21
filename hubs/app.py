bbdc99a2902da8235434c126dda74743aea8bc91
Ralph Bean <rbean@redhat.com>
2015-05-21 12:43:26 -0400

    Provide a 302 to link to the source code for widgets.

--- hubs/app.py
+++ hubs/app.py
@@ -7,6 +7,12 @@ import models
 app = flask.Flask(__name__)
 
 
+# TODO - put this in config so we can migrate to pagure
+# TODO - instead of 'develop', use the version from pkg_resources to figure out
+#        the right tag to link people to.  AGPL ftw.
+SOURCE_URL = 'https://github.com/ralphbean/fedora-hubs-prototype/blob/develop'
+
+
 app.config.from_object('default_config')
 if 'HUBS_CONFIG' in os.environ:
     app.config.from_envvar('HUBS_CONFIG')
@@ -17,15 +23,57 @@ def index():
     return flask.redirect('/designteam')  # TODO - remove this later
 
 
-@app.route('/<hub_name>')
-@app.route('/<hub_name>/')
-def hubs_view(hub_name):
+@app.route('/<name>')
+@app.route('/<name>/')
+def hub(name):
     session = models.init(app.config['DB_URL'])
     hub = session.query(models.Hub)\
-        .filter(models.Hub.name==hub_name)\
+        .filter(models.Hub.name==name)\
         .first()
 
+    if not hub:
+        flask.abort(404)
+
     return flask.render_template('hubs.html', hub=hub, session=session)
+
+
+def get_widget(session, hub, idx):
+    try:
+        idx = int(idx)
+    except TypeError:
+        flask.abort(404)
+
+    hub = session.query(models.Hub)\
+        .filter(models.Hub.name==hub)\
+        .first()
+
+    if not hub:
+        flask.abort(404)
+
+    for widget in hub.widgets:
+        if widget.idx == idx:
+            return widget
+
+    flask.abort(404)
+
+
+@app.route('/<hub>/<idx>')
+@app.route('/<hub>/<idx>/')
+def widget(hub, idx):
+    session = models.init(app.config['DB_URL'])
+    widget = get_widget(session, hub, idx)
+    return widget.render(session, hub, idx)
+
+
+@app.route('/<hub>/<idx>/source')
+@app.route('/<hub>/<idx>/source/')
+def widget_source(hub, idx):
+    from hubs.widgets import registry
+    base = '/hubs/'
+    session = models.init(app.config['DB_URL'])
+    widget = get_widget(session, hub, idx)
+    fname = base + registry[widget.plugin].__file__.split(base, 1)[1][:-1]
+    return flask.redirect(SOURCE_URL + fname)
 
 
 if __name__ == '__main__':
