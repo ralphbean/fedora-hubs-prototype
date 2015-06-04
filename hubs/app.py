626f7b7830d06a021f5e8b4e3cd8698d7ad4b0d4
Ralph Bean <rbean@redhat.com>
2015-06-04 14:27:32 -0400

    Messing with authn.

--- hubs/app.py
+++ hubs/app.py
@@ -1,6 +1,9 @@
+import functools
 import os
 
 import flask
+
+from flask.ext.openid import OpenID
 
 import hubs.models
 
@@ -20,6 +23,8 @@ if 'HUBS_CONFIG' in os.environ:
 import fedmsg.config
 fedmsg_config = fedmsg.config.load_config()
 
+session = hubs.models.init(fedmsg_config['hubs.sqlalchemy.uri'])
+
 
 @app.route('/')
 def index():
@@ -29,7 +34,6 @@ def index():
 @app.route('/<name>')
 @app.route('/<name>/')
 def hub(name):
-    session = hubs.models.init(fedmsg_config['hubs.sqlalchemy.uri'])
     hub = session.query(hubs.models.Hub)\
         .filter(hubs.models.Hub.name==name)\
         .first()
@@ -63,7 +67,6 @@ def get_widget(session, hub, idx):
 @app.route('/<hub>/<idx>')
 @app.route('/<hub>/<idx>/')
 def widget_render(hub, idx):
-    session = hubs.models.init(fedmsg_config['hubs.sqlalchemy.uri'])
     widget = get_widget(session, hub, idx)
 
     # Make this artificially slow for development...
@@ -78,7 +81,6 @@ def widget_render(hub, idx):
 @app.route('/<hub>/<idx>/json')
 @app.route('/<hub>/<idx>/json/')
 def widget_json(hub, idx):
-    session = hubs.models.init(fedmsg_config['hubs.sqlalchemy.uri'])
     widget = get_widget(session, hub, idx)
     from hubs.widgets import registry
     module = registry[widget.plugin]
@@ -100,6 +102,88 @@ def widget_source(name):
     base = '/hubs/'
     fname = base + registry[name].__file__.split(base, 1)[1][:-1]
     return flask.redirect(SOURCE_URL + fname)
+
+
+# Set up OpenID in stateless mode
+oid = OpenID(app,
+             safe_roots=[],
+             store_factory=lambda: None,
+             url_root_as_trust_root=True)
+
+
+@app.route('/login/', methods=('GET', 'POST'))
+@app.route('/login', methods=('GET', 'POST'))
+@oid.loginhandler
+def login():
+    default = flask.url_for('index')
+    next_url = flask.request.args.get('next', default)
+    if flask.g.auth.logged_in:
+        return flask.redirect(next_url)
+
+    openid_server = flask.request.form.get('openid', None)
+    if openid_server:
+        return oid.try_login(
+            openid_server, ask_for=['email', 'fullname', 'nickname'],
+            ask_for_optional=[])
+
+    return flask.render_template(
+        'login.html', next=oid.get_next_url(), error=oid.fetch_error())
+
+
+@app.route('/login/fedora/')
+@app.route('/login/fedora')
+@oid.loginhandler
+def fedora_login():
+    #default = flask.url_for('profile_redirect')
+    #next_url = flask.request.args.get('next', default)
+    return oid.try_login(
+        'https://id.fedoraproject.org',
+        ask_for=['email', 'fullname', 'nickname'],
+        ask_for_optional=[])
+
+
+@app.route('/logout/')
+@app.route('/logout')
+def logout():
+    if 'openid' in flask.app.session:
+        flask.app.session.pop('openid')
+    return flask.redirect(flask.url_for('index'))
+
+
+@oid.after_login
+def after_openid_login(resp):
+    default = flask.url_for('index')
+    if not resp.identity_url:
+        return flask.redirect(default)
+
+    openid_url = resp.identity_url
+    flask.app.session['openid'] = openid_url
+    flask.app.session['fullname'] = resp.fullname
+    flask.app.session['nickname'] = resp.nickname or resp.fullname
+    flask.app.session['email'] = resp.email
+    next_url = flask.request.args.get('next', default)
+    return flask.redirect(next_url)
+
+
+def login_required(function):
+    """ Flask decorator to restrict access to logged-in users. """
+    @functools.wraps(function)
+    def decorated_function(*args, **kwargs):
+        """ Decorated function, actually does the work. """
+        if not flask.g.auth.logged_in:
+            flask.flash('Login required', 'errors')
+            return flask.redirect(flask.url_for(
+                'fedora_login', next=flask.request.url))
+
+        # TODO - Ensure that the logged in user exists before we proceed.
+        hubs.models.User.get_or_create(
+            session,
+            openid=flask.g.auth.openid,
+            openid_url=flask.g.auth.openid_url,
+        )
+
+        return function(*args, **kwargs)
+    return decorated_function
 
 
 if __name__ == '__main__':
