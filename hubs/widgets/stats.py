747a94a3042bfd95f427989795bd88f1197088b9
Remy DeCausemaker <decause@redhat.com>
2015-06-07 17:34:36 -0400

    Remove stats-table style from widget and put into site.css

--- hubs/widgets/stats.py
+++ hubs/widgets/stats.py
@@ -11,23 +11,6 @@ template = jinja2.Template("""
   </table>
   <div class="pull-right"><button class="btn btn-info">Subscribe</button></div>
 </div>
-<style>
-  .stats-table {
-    width: 180px
-    padding: 0px;
-    margin: 0px;
-    display: inline-block;
-  }
-  .stats-table th {
-    text-transform: uppercase;
-    font-size: 80%;
-    padding-right: 10px;
-    color: #797a7c;
-  }
-  .stats-table td {
-    font-size: 32pt;
-  }
-</style>
 """)
 
 
