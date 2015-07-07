50b9c718614aa0f19ee2fe18cfdb2a732583ae44
Remy DeCausemaker <decause@redhat.com>
2015-07-07 15:11:28 -0400

    Added internationalization team stub

--- populate.py
+++ populate.py
@@ -11,13 +11,53 @@ fedmsg_config = fedmsg.config.load_config()
 session = hubs.models.init(fedmsg_config['hubs.sqlalchemy.uri'], True, True)
 
 users = ['mrichard', 'duffy', 'ryanlerch', 'gnokii', 'nask0',
-         'abompard', 'decause', 'ralph', 'lmacken', 'croberts', 'mattdm']
+         'abompard', 'decause', 'ralph', 'lmacken', 'croberts', 'mattdm',
+         'pravins']
 for username in users:
     fullname = 'Full Name Goes Here'
     openid = '%s.id.fedoraproject.org' % username
     print("Creating account for %r" % openid)
     hubs.models.User.get_or_create(
         session, openid=openid, fullname=fullname)
+
+session.commit()
+
+############## Internationalizationteam
+hub = hubs.models.Hub(name='Internationalizationteam', summary='The Fedora Internationalization Team')
+session.add(hub)
+
+widget = hubs.models.Widget(plugin='stats', index=0)
+hub.widgets.append(widget)
+widget = hubs.models.Widget(plugin='rules', index=1,
+                            _config=json.dumps({
+                                'link': 'https://fedoraproject.org/wiki/I18N'
+                            }))
+hub.widgets.append(widget)
+
+widget = hubs.models.Widget(plugin='dummy', index=2)
+hub.widgets.append(widget)
+
+# Added a hubs about widget
+widget = hubs.models.Widget(plugin='about', index=3,
+                            _config=json.dumps({
+                                "text": "I'm a Fedora user, and this is my about widget text!",
+                            }))
+hub.widgets.append(widget)
+
+widget = hubs.models.Widget(plugin='sticky', index=0, left=True,
+                            _config=json.dumps({
+                                'text': "<p>The Fedora I18N project works on internationalization (i18n) to support the localization (l10n) of Fedora in many languages. Translation of Fedora software and documentation are handled by the Fedora L10N project.</p> <p>The goals of the Project are to:</p> <ul><li>Develop, package, and maintain applications like input methods for different languages</li> <li>Improve applications and utilities to support and process different languages</li> <li>Quality-assure that existing applications meet i18n standards</li> <li>Support the infrastructure of the Fedora Localization Project</li></ul>",
+                            }))
+hub.widgets.append(widget)
+widget = hubs.models.Widget(plugin='dummy', index=1, left=True)
+hub.widgets.append(widget)
+
+
+# Set up some memberships
+hub.subscribe(session, hubs.models.User.by_openid(session, 'pravins.id.fedoraproject.org'), 'owner')
+hub.subscribe(session, hubs.models.User.by_openid(session, 'decause.id.fedoraproject.org'), 'member')
+hub.subscribe(session, hubs.models.User.by_openid(session, 'ralph.id.fedoraproject.org'), 'subscriber')
+
 
 session.commit()
 
